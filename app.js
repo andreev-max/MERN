b0e0ef6be7932c2e68a0bcff4a0208d2ea649abe
nAzdAc <nAzdAc15071996@mail.ru>
2021-03-10 15:23:43 +0300

    feat: add login, registration, CRA client

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('init')
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use('/api/auth', require('./routes/auth.routes'))
+
+const PORT = config.get('port') || 8080;
+
+async function start() {
+	try {
+		await mongoose.connect(config.get('mongoUri'), {
+			useCreateIndex: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+		app.listen(PORT, () => {
+			console.log(`Server has been started on port ${PORT}`);
+		});
+	} catch (e) {
+		console.log('Server error', e.message);
+    process.exit(1)
+	}
+}
+
+start();
